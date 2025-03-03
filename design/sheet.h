6652fce09f792a06f4a6987f659da462d0995eb6
Alexander <142852639+AlexanderBalunov@users.noreply.github.com>
2025-03-04 01:12:40 +0300

    Update sheet.h

--- design/sheet.h
+++ design/sheet.h
@@ -7,8 +7,6 @@
 
 class Sheet : public SheetInterface {
 public:
-    ~Sheet();
-
     void SetCell(Position pos, std::string text) override;
 
     const CellInterface* GetCell(Position pos) const override;
@@ -21,14 +19,6 @@ public:
     void PrintValues(std::ostream& output) const override;
     void PrintTexts(std::ostream& output) const override;
 
-    const Cell* GetConcreteCell(Position pos) const;
-    Cell* GetConcreteCell(Position pos);
-
 private:
-    void MaybeIncreaseSizeToIncludePosition(Position pos);
-    void PrintCells(std::ostream& output,
-                    const std::function<void(const CellInterface&)>& printCell) const;
-    Size GetActualSize() const;
-
-    std::vector<std::vector<std::unique_ptr<Cell>>> cells_;
+	std::vector<std::vector<std::unique_ptr<Cell>>> table_;
 };
